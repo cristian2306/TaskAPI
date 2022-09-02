14dabf952b03bef093b211af1569986a477d7efb
cristian2306 <cristian.castellanos@mail.escuelaing.edu.co>
2022-09-01 19:28:46 -0500

    Repo creado

---
+++ src/test/java/edu/eci/escuelaing/ieti/taskapi/TaskApiApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.eci.escuelaing.ieti.taskapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
